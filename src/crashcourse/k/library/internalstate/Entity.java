1bcdbaf8bd409a0fe202e997a3c2043760b66e69
Kenzie Togami <ket1999@gmail.com>
2013-10-18 23:02:34 -0700

    Added motion and position

--- src/crashcourse/k/library/internalstate/Entity.java
+++ src/crashcourse/k/library/internalstate/Entity.java
@@ -1,5 +1,25 @@
 package crashcourse.k.library.internalstate;
 
 public abstract class Entity {
-
+	public float posX, posY, posZ;
+	public float motionX, motionY, motionZ;
+	
+	/**
+	 * Attempts to move with the current motion X-Z values
+	 */
+	public void attemptMove() {
+		move(motionX, motionY, motionZ);
+	}
+	
+	/**
+	 *  Moves with the given motion X-Z values
+	 * @param mX - the motionX
+	 * @param mY - the motionY
+	 * @param mZ - the motionZ
+	 */
+	public void move(float mX, float mY, float mZ) {
+		posX += mX;
+		posY += mY;
+		posZ += mZ;
+	}
 }
